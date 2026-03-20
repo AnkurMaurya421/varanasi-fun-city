8700d92ecdc3c7889255839e35fa57508152038a
AnkurMaurya421 <ankurmaurya421@gmail.com>
2026-03-20 13:35:40 +0530

    worked on Navigation bar for mobiles

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,16 +1,47 @@
+"use client"
 import styles from "./Navbar.module.css";
-import Image from "next/image";
+import {useState} from "react";
+import { Menu } from "lucide-react";
+import { CircleX } from "lucide-react";
 export default function Navbar() {
-    return (
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+    return (<>
         <nav className={styles.navbar}>
             <div className={styles.logo}>
                 Varanasi <span >Fun City</span>
             </div>
             <ul className={styles.navLinks}>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Rides</a></li>
+                <li><a href="/">Pricing</a></li>
+                <li><a href="/">Timing</a></li>
+                <li><a href="/">Gallery</a></li>
+                <li><a href="/">Events</a></li>
+                <li><a href="/">Contact</a></li>
+                <li><button className={styles.bookBtn}>Book Tickets</button></li>
+                
             </ul>
+            {isMenuOpen ? (
+                <CircleX className={styles.menuBtn} onClick={() => setIsMenuOpen(!isMenuOpen)} />
+            ) : (
+                <Menu className={styles.menuBtn} onClick={() => setIsMenuOpen(!isMenuOpen)} />
+            )}
         </nav>
+        {isMenuOpen?
+        <nav className={styles.mobileNav}>
+            <ul className={styles.mobileNavLinks}> 
+                <li><a href="/">About</a></li>
+                <li><a href="/">Rides</a></li>
+                <li><a href="/">Pricing</a></li>
+                <li><a href="/">Timing</a></li>
+                <li><a href="/">Gallery</a></li>
+                <li><a href="/">Events</a></li>
+                <li><a href="/">Contact</a></li>
+                <li><button className={styles.bookBtn}>Book Tickets</button></li>
+            </ul>
+        </nav>:null}
+    </>
     )
 }
